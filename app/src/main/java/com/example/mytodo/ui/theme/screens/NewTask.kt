6441039b70200b9612afddf0c88d9fb920633d50
claudio2512 <claudio2512@gmail.com>
2024-07-08 11:54:53 -0300

    second commit

--- app/src/main/java/com/example/mytodo/ui/theme/screens/NewTask.kt
+++ app/src/main/java/com/example/mytodo/ui/theme/screens/NewTask.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,25 +22,20 @@ import com.example.mytodo.components.MyMenuItens
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun NewTask(){
+fun NewTask() {
 
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-
         topBar = {
             CenterAlignedTopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
+                    containerColor = Color.White,
+                    titleContentColor = Color.Black,
                 ),
                 title = {
-                    Text(
-                        "Nova Task",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
+                    Text("Nova Task")
                 },
                 navigationIcon = {
                     Text(text = " Cancelar ")
@@ -52,10 +48,11 @@ fun NewTask(){
         },
     ) { innerPadding ->
         Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier
                 .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
+
+            ) {
             //....
         }
     }
